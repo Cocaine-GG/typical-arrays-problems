cd27371611b0446f0ad50c8e7045569a1ed6dad4
Daniel Kondratiuk <venga1.ua@gmail.com>
2021-08-17 23:10:52 +0200

    feat: implemented functions min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.min = min = (array) => (!array ||array.length === 0) ? 0 : Math.min(...array)
+exports.max = max = (array) => (!array ||array.length === 0) ? 0 : Math.max(...array)
+exports.avg = avg = (array) => (!array ||array.length === 0) ? 0 : array.reduce((a,b)=>a+b,0)/array.length
